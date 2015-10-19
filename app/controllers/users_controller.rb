5683e79c5c84d71256e6db40ed3d996634f7cbbc
Lisa Folliard <lisaf37@gmail.com>
2015-10-19 16:18:00 -0700

    update users controller so users can sign up and be logged in automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the site!"
     redirect_to "/"
     else
