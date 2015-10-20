0f83c81a623dd7fbaaa908db96a11e61d1f61d6d
Guest User <student@epicodus.com>
2015-10-20 12:00:30 -0700

    pass integration spec to test adding a new user process

--- spec/features/add_question_pages_spec.rb
+++ spec/features/add_question_pages_spec.rb
@@ -13,34 +13,17 @@ describe "the add a question process" do
 end
 
 # describe "the update a question process" do
-#   it "update a question" do
-#     visit questions_path
-#     click_on "New Question"
-#     fill_in 'Title', :with => 'Install Node'
-#     fill_in 'Description', :with => 'Issues installing'
-#     fill_in 'Author', :with => 'A movie with Guy Ritchie'
-#     click_on 'Create Question'
-#     click_on 'Install Node'
-#     click_on 'Update Question'
-#     fill_in 'Title', :with => 'Install Node.js'
-#     fill_in 'Description', :with => 'Issues installing'
-#     fill_in 'Author', :with => 'A movie with Guy Ritchie'
-#     click_on 'Update Question'
-#     expect(page).to have_content 'Install Node.js'
-#   end
-# end
-
-# describe "the update a question process" do
 #   it "updates a question" do
+#
 #     question = Question.create(:title => 'Install Node', :description => 'Problem', :author => 'Joe Jones')
 #     response = Response.create(:response => 'Download it', :name => 'Mary', :question_id => question.id)
 #     visit question_path(question)
+#     click_on "Update Question"
+#     fill_in save_and_open_page
+#     fill_in 'Description', :with => 'Issues installing'
+#     fill_in 'Author', :with => 'A movie with Guy Ritchie'
 #     click_on 'Update Question'
-#     fill_in 'Title', :with => "Install Node.js"
-#     fill_in 'Description', :with => "Download"
-#     fill_in 'Author', :with => "Mary"
-#     click_on 'Update Question'
-#     expect(page).to have_content 'Install Node.js'
+#     expect(page).to have_content "Install Node.js"
 #   end
 # end
 
---
+++ spec/features/add_user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'add a user process' do
+  it 'can add a user' do
+    visit new_user_path
+    click_on 'Sign Up'
+    fill_in 'Email', :with => 'joe@blow.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Click Here'
+    expect(page).to have_content 'joe@blow.com' # takes user to sign in page
+  end
+end
