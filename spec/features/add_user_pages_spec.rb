804a1fd55ee58cbc359c0d7b598442d2b4502f24
Guest User <student@epicodus.com>
2015-10-20 12:17:20 -0700

    pass integration spec to test display of error message when log in form is incomplete

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -18,4 +18,22 @@ describe 'add a user process' do
     click_button 'Click Here'
     expect(page).to have_content "There was a problem creating your account"
   end
+
+  # it 'allows a user to log in' do
+  #   visit new_user_path
+  #   click_on "Log In"
+  #   fill_in 'Email', :with => 'bob@bob.com'
+  #   fill_in 'Password', :with => '55555'
+  #   click_button "Log in"
+  #   expect(page).to have_content 'logged in'
+  # end
+
+  it 'displays error if user does not log in correctly' do
+    visit new_user_path
+    click_on "Log In"
+    fill_in 'Email', :with => 'joe@blow.com'
+    fill_in 'Password', :with => ''
+    click_button "Log in"
+    expect(page).to have_content 'There was a problem logging you in'
+  end
 end
